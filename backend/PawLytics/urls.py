edb0080dd71737ebe58d920e18dfbbab26b9924e
VetalyaRabotyaga <108223842+VetalKrut1337@users.noreply.github.com>
2025-06-08 18:40:29 +0300

    add authorization
    add register
    add login
    add swagger

--- backend/PawLytics/urls.py
+++ backend/PawLytics/urls.py
@@ -17,6 +17,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+
+# Swagger:
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="PawLytics API",
+      default_version='v1',
+      description="Документація API для реєстрації, логіну, готелів і аналітики",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('users.urls')),  # <-- путь к регистрации/логину
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
