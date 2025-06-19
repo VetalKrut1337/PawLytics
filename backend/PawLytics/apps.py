198f9a2574c77c17ab3c08a2b53a2c59f570eb32
VetalyaRabotyaga <108223842+VetalKrut1337@users.noreply.github.com>
2025-06-19 22:23:14 +0300

    rename pawlytics

--- backend/PawLytics/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class PawlyticsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pawlytics'
-
-    def ready(self):
-        import pawlytics.signals
